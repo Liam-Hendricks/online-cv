e1dcbac1f4104bdf7edae3604c9c76ba107c6e08
Liam Hendricks <liamkeatonhendricks@gmail.com>
2020-08-26 20:19:29 +0200

    added animation to skills

--- pages/projects.js
+++ pages/projects.js
@@ -12,7 +12,10 @@ const projects = () => {
           <Carousel interval={null}>
             <Carousel.Item>
               <div className="carousel-img">
-                <img src="../static/minesweeper/mine_image2.png" alt="First slide" />
+                <img
+                  src="../static/minesweeper/mine_image2.png"
+                  alt="First slide"
+                />
               </div>
 
               <Carousel.Caption>
@@ -22,8 +25,11 @@ const projects = () => {
             </Carousel.Item>
             <Carousel.Item>
               <div className="carousel-img">
-                <img className="img-carousel" src="../static/minesweeper/mine_image3.png" alt="First slide" />
-                             
+                <img
+                  className="img-carousel"
+                  src="../static/minesweeper/mine_image3.png"
+                  alt="First slide"
+                />
               </div>
 
               <Carousel.Caption>
@@ -33,31 +39,32 @@ const projects = () => {
             </Carousel.Item>
             <Carousel.Item>
               <div className="carousel-img">
-                <img src="../static/minesweeper/mine_image1.png" alt="First slide" />
+                <img
+                  src="../static/minesweeper/mine_image1.png"
+                  alt="First slide"
+                />
               </div>
-              <Carousel.Caption >
+              <Carousel.Caption>
                 <h3>Help Menu</h3>
-                <p>
-                 Click on help to learn the rules
-                </p>
+                <p>Click on help to learn the rules</p>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
         </div>
         <div className="right-container">
           <div className="right-text">
-            <h4>MineSweeper using React and hosted on Heroku</h4> 
-            <br/>          
+            <h4>MineSweeper using React and hosted on Heroku</h4>
+            <br />
             <p>
-            This is one of my capstone projects after learning how to use react.
+              This is one of my capstone projects after learning how to use
+              react.
               <br />
               The task was to building a gaming using react.
               <br />
-              
               I chose to build minesweeper.
               <br />
-              This project was challenging but once I figured out the main logic,
-              it all came together.
+              This project was challenging but once I figured out the main
+              logic, it all came together.
               <br />
               After testing and debugging this app ,I deployed and hosted it on
               Heroku.
@@ -68,97 +75,113 @@ const projects = () => {
         </div>
       </section>
       <section className="Itunes-Library">
-      <div className="left-container">
+        <div className="left-container">
           <Carousel interval={null}>
             <Carousel.Item>
               <div className="carousel-img">
-                <img src="../static/Itunes/itunes_image1.png" alt="First slide" />
+                <img
+                  src="../static/Itunes/itunes_image1.png"
+                  alt="First slide"
+                />
               </div>
 
               <Carousel.Caption>
-              <h3>Store Favourite Songs</h3>
+                <h3>Store Favourite Songs</h3>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <div className="carousel-img">
-                <img className="img-carousel" src="../static/Itunes/itunes_image2.png" alt="First slide" />
-                             
+                <img
+                  className="img-carousel"
+                  src="../static/Itunes/itunes_image2.png"
+                  alt="First slide"
+                />
               </div>
 
               <Carousel.Caption>
-              <h3>Filter by Category and search</h3>
+                <h3>Filter by Category and search</h3>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <div className="carousel-img">
-                <img src="../static/Itunes/itunes_image3.png" alt="First slide" />
+                <img
+                  src="../static/Itunes/itunes_image3.png"
+                  alt="First slide"
+                />
               </div>
-              <Carousel.Caption >
-              <h3>Search results are updated while you type</h3>
+              <Carousel.Caption>
+                <h3>Search results are updated while you type</h3>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
         </div>
         <div className="right-container">
           <div className="right-text">
-              <h4>Itunes Library API with React hosted on Heroku</h4>
-              <br/>     
-              <p>
-                  In this CapStone Project I applied my knowledge of using
-                  third-party api's with react and express.
-                  <br />
-                  This project uses the Itunes api and allows the user to search for
-                  media using different search catergories.
-                  <br />
-                  It also allows the user to store media they like on a favourites
-                  list which they can view.This project was also deployed on Heroku.
-                  <br />
-                  Click on 'try it out here' to see for yourself.
-                </p>
+            <h4>Itunes Library API with React hosted on Heroku</h4>
+            <br />
+            <p>
+              In this CapStone Project I applied my knowledge of using
+              third-party api's with react and express.
+              <br />
+              This project uses the Itunes api and allows the user to search for
+              media using different search catergories.
+              <br />
+              It also allows the user to store media they like on a favourites
+              list which they can view.This project was also deployed on Heroku.
+              <br />
+              Click on 'try it out here' to see for yourself.
+            </p>
           </div>
-
         </div>
       </section>
       <section className="Drone-Flight-Planner">
         <div className="left-container">
-            <Carousel interval={null}>
-              <Carousel.Item>
-                <div className="carousel-img">
-                  <img src="../static/RPAS_app/drone_image2.png" alt="First slide" />
-                </div>
+          <Carousel interval={null}>
+            <Carousel.Item>
+              <div className="carousel-img">
+                <img
+                  src="../static/RPAS_app/drone_image2.png"
+                  alt="First slide"
+                />
+              </div>
 
-                <Carousel.Caption>
-                  <h3>Drone Flight Planner</h3>
-                  <p>Login with facebook or create account</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <div className="carousel-img">
-                  <img className="img-carousel" src="../static/RPAS_app/drone_image1.png" alt="First slide" />
-
-                </div>
-                <Carousel.Caption>
-                  <h3>Dashboard</h3>
-                  <p>See active events and get latest weather</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <div className="carousel-img">
-                  <img src="../static/RPAS_app/drone_image3.png" alt="First slide" />
-                </div>
-                <Carousel.Caption >
-                  <h3>Document Upload</h3>
-                  <p>Upload or Load documents</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
-          </div>
+              <Carousel.Caption>
+                <h3>Drone Flight Planner</h3>
+                <p>Login with facebook or create account</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <div className="carousel-img">
+                <img
+                  className="img-carousel"
+                  src="../static/RPAS_app/drone_image1.png"
+                  alt="First slide"
+                />
+              </div>
+              <Carousel.Caption>
+                <h3>Dashboard</h3>
+                <p>See active events and get latest weather</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <div className="carousel-img">
+                <img
+                  src="../static/RPAS_app/drone_image3.png"
+                  alt="First slide"
+                />
+              </div>
+              <Carousel.Caption>
+                <h3>Document Upload</h3>
+                <p>Upload or Load documents</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
         <div className="right-container">
           <div className="right-text">
-
-          <h4>Drone Planner App using MERN stack hosted on Heroku</h4>
-          <br/>   
-          <p>
+            <h4>Drone Planner App using MERN stack hosted on Heroku</h4>
+            <br />
+            <p>
               This is my final Cap Stone Project of my bootcamp ,where I apply
               everything I have learnt in the MERN stack.
               <br />
@@ -169,11 +192,10 @@ const projects = () => {
               <br />
               It allows user to plan drone flights and have all the nessessary
               documents.
-              <br />
-              A user also has access to weather data using their gps
+              <br />A user also has access to weather data using their gps
               location or can get weather data at a location of a planned
               flight.
-            </p>    
+            </p>
           </div>
         </div>
       </section>
--- pages/skills.js
+++ pages/skills.js
@@ -1,8 +1,46 @@
 import Layout from "../components/MyLayout";
 
 const skills=()=>{
-  return(<Layout>
-
+  return(
+  <Layout>
+      <div className="skills-container">
+      <div className="skills-grid">
+        <sec className="skill html">
+          <div className="icon">
+            <i className="fab fa-html5"/>
+          </div>
+        </sec>
+        <sec className="skill css">
+          <div className="icon">
+            <i className="fab fa-css3-alt"/>
+          </div>
+        </sec>
+        <sec className="skill js">
+          <div className="icon">
+            <i className="fab fa-js-square"/>
+          </div>
+        </sec>
+        <sec className="skill react">
+          <div className="icon">
+            <i className="fab fa-react"/>
+          </div>
+        </sec>
+        <sec className="skill node">
+          <div className="icon">
+            <div className="icon-holder">
+            < i className="fab fa-node"/>  
+            </div>
+            
+          </div>
+        </sec>
+        <sec className="skill git">
+          <div className="icon">
+            <i className="fab fa-github-square"/>
+          </div>
+        </sec>
+      </div>
+      </div>
+    
 
   </Layout>);
 
